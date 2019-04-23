61d3cbffbefaa7941e7d59944baba86dbba45fb1
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-23 17:09:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,8 @@
 def reverse_each_word(string)
-sentence1.reverse_each do {|sentence1| puts sentence1}
-end
-def reverse_each_word(string)
   array = string.split(" ")
   parray = []
   array.collect do|string|
-    our_array << string.reverse
+    parray << string.reverse
   end
-  our_array.join(" ")
+  parray.join(" ")
 end
