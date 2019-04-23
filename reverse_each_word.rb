5d78667c0eda1d7ca99d380de2e49fd3de9de6c9
Jhana Gustave <jhana.gustave@gmail.com>
2019-04-23 17:06:44 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,11 @@
-sentence1 = "Hello there, and how are you?"
-def reverse_each_word(sentence1)
+def reverse_each_word(string)
 sentence1.reverse_each do {|sentence1| puts sentence1}
+end
+def reverse_each_word(string)
+  array = string.split(" ")
+  our_array = []
+  array.collect do|string|
+    our_array << string.reverse
+  end
+  our_array.join(" ")
 end
